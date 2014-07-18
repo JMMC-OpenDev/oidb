d2ac5152009d9d505bf4077050da04ed4cc84cf9
ptrckb <patrick.bernaud@univ-grenoble-alpes.fr>
2014-07-18 11:10:38 +0000

    Add SAMP menu for visualizing OIFITS files at validation time.

--- resources/scripts/collection-vizier.js
+++ resources/scripts/collection-vizier.js
@@ -77,6 +77,14 @@ $(function () {
         e.preventDefault();
     });
 
+    $('tr .dropdown').sampify(
+        'table.load.fits',
+        // prepare parameters for the 'table.load.fits'
+        function () {
+            // set SAMP parameter to URL of the relevant OIFITS file
+            return { "url": $(this).siblings('a').attr('href') };
+        });
+
     // Drop-down list of instrument:
     // event handler for updating the list of mode on changes to the instrument
     $(':input[name="instrument_name"]').change(function (e) {
