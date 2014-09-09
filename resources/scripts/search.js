d0d25c6f4143bd4e4b5876df7f9e6a9b11db3c26
ptrckb <patrick.bernaud@univ-grenoble-alpes.fr>
2014-09-09 14:59:11 +0000

    Save real-estate for results with collapsible filters form on the search page.

--- resources/scripts/search.js
+++ resources/scripts/search.js
@@ -3,4 +3,23 @@ $(function () {
         autoclose: true,
         format: 'yyyy-mm-dd'
     });
+
+    // the panel with filters fields
+    var $filters = $('#filters');
+
+    function toggleChevron(e) {
+        $(e.target)
+            .prev('.panel-heading')
+            .find("i.indicator")
+            .toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
+    }
+    // connect the icon in heading with the state of the panel
+    $filters.on('hidden.bs.collapse shown.bs.collapse', toggleChevron);
+
+    // collapse filters if not on the first page of the search
+    // (i.e. the filters have already been set)
+    var page = new RegExp('[?&]page=([^&#]*)').exec(window.location.href);
+    if (page && page[1] != 1) {
+        $filters.collapse('hide');
+    }
 });
