76a80b31c79d42ef100b49056aadae2cd5703905
ptrckb <patrick.bernaud@univ-grenoble-alpes.fr>
2014-12-11 15:27:49 +0000

    Use AJAX pagination for granules on collection page.

--- resources/scripts/collection.js
+++ resources/scripts/collection.js
@@ -19,4 +19,18 @@ $(function () {
             // set SAMP parameter to URL of the relevant OIFITS file
             return { "url": $(this).siblings('a').attr('href') };
         });
+
+    // AJAX pagination of granules
+    $("#granules" ).on( "click", ".pager a", function(e) {
+        e.preventDefault();
+
+        var query = $(this).attr('href').split('?')[1];
+        
+        $('#granules .pager li').addClass('disabled');
+
+        // replace results
+        $('#granules')
+            .find('> div').fadeTo('fast', 0.5).end()
+            .load('_collection-granules.html' + '?' + query);
+    });
 });
