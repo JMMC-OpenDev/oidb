df5d01b8328049f3a74ab424dc053624759fad38
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2015-05-19 07:25:28 +0000

    include admin.js for superusers host by-nc-sa logo in local resources

---
+++ resources/scripts/admin.js
@@ -0,0 +1,28 @@
+$(function () {
+    
+    $("button[data-colref]").click(function(e) {
+        e.preventDefault();
+        var id= $(this).attr("data-colref");
+        var answer = confirm('Are you sure you want to delete the "'+id+'" collection ?');
+        if (answer)
+        {
+            deleteCollection(id, $(this).parents("a"));
+        }
+        else
+        {
+          console.log('cancel');
+        }
+    });
+    
+    function deleteCollection(id, $blockToRemove) {
+        $.ajax(
+            'restxq/oidb/collection/'+id, { type: 'DELETE' }
+        ).done(function() {
+            $blockToRemove.remove();
+        }).fail(function( jqXHR, textStatus, errorThrown ) {
+            alert( "Request failed: " + textStatus + " ( " + errorThrown + " )");
+        });
+    }
+
+                  
+});
