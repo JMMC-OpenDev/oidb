c0372fce533b7189e14d7c12a6bdf4cda58252c5
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2020-06-02 07:02:10 +0000

    add interferometer_stations

--- conf/oidb.sql
+++ conf/oidb.sql
@@ -22,62 +22,64 @@ CREATE TABLE oidb (
     -- ObsCore model: observation information
     dataproduct_type  text,
     -- TODO 0,1,2,3
-    calib_level       integer NOT NULL,
+    calib_level             integer NOT NULL,
     -- ObsCore model: target information
-    target_name       text NOT NULL,
+    target_name             text NOT NULL,
     -- ObsCore model: data description
-    obs_id            text,
-    obs_collection    text,
-    obs_creator_name  text,
+    obs_id                  text,
+    obs_collection          text,
+    obs_creator_name        text,
     -- ObsCore model: curation information
-    obs_release_date  timestamp without time zone DEFAULT now(),
-    obs_publisher_did text,
-    bib_reference     text,
-    data_rights       rights DEFAULT 'public'::text NOT NULL,
+    obs_release_date        timestamp without time zone DEFAULT now(),
+    obs_publisher_did       text,
+    bib_reference           text,
+    data_rights             rights DEFAULT 'public'::text NOT NULL,
     -- ObsCore model: access information
-    access_url        text NOT NULL,
-    access_format     text,
-    access_estsize    bigint,
+    access_url              text NOT NULL,
+    access_format           text,
+    access_estsize          bigint,
     -- ObsCore model: spatial characterisation
-    s_ra              double precision,
-    s_dec             double precision,
-    s_fov             real,
-    s_region          real,
-    s_resolution      real,
+    s_ra                    double precision,
+    s_dec                   double precision,
+    s_fov                   real,
+    s_region                real,
+    s_resolution            real,
     -- ObsCore model: time characterisation
-    t_min             real,
-    t_max             real,
-    t_exptime         real,
-    t_resolution      real,
+    t_min                   real,
+    t_max                   real,
+    t_exptime               real,
+    t_resolution            real,
     -- ObsCore model: spectral characterisation
-    em_min            real,
-    em_max            real,
-    em_res_power      real,
+    em_min                  real,
+    em_max                  real,
+    em_res_power            real,
     -- ObsCore model: observable axis (left NULL as OIFits contains several observable quantities VIS, VIS2, T3)
-    o_ucd             text,
+    o_ucd                   text,
     -- ObsCore model: polarisation axis
-    pol_states        text,
+    pol_states              text,
     -- ObsCore model: provenance
-    facility_name     text,
-    instrument_name   text,
+    facility_name           text,
+    instrument_name         text,
     -- OiDB Extension (OIFits metadata)
-    instrument_mode   text,
+    instrument_mode         text,
     -- TODO 0,1,2,3,4
-    quality_level     integer,
- 
-    nb_channels       integer NOT NULL,
-    nb_vis            integer,
-    nb_vis2           integer,
-    nb_t3             integer,
-    keywords          text,
+    quality_level           integer,
 
-    subdate           timestamp without time zone DEFAULT now(),
-    id                bigint DEFAULT nextval('oidb_id_seq') PRIMARY KEY,
+    -- Our extension (we could have put a prefix...) 
+    interferometer_stations text,
+    nb_channels             integer NOT NULL,
+    nb_vis                  integer,
+    nb_vis2                 integer,
+    nb_t3                   integer,
+    keywords                text,
 
-    progid            text,
-    datapi            text,
+    subdate                 timestamp without time zone DEFAULT now(),
+    id                      bigint DEFAULT nextval('oidb_id_seq') PRIMARY KEY,
 
-    access_md5        VARCHAR(32)
+    progid                  text,
+    datapi                  text,
+
+    access_md5              VARCHAR(32)
 
 );
 
