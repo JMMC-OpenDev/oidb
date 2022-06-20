a73336ef0957983a462e991582b6d1ad273d7e24
gmella <guillaume.mella@univ-grenoble-alpes.fr>
2022-06-20 07:12:11 +0000

    add note

--- conf/complete_oidb_obscore_schema.sql
+++ conf/complete_oidb_obscore_schema.sql
@@ -1,9 +1,13 @@
 ALTER TABLE public.oidb ADD dataproduct_category text NULL;
 ALTER TABLE public.oidb ADD proposal_subid text NULL;
+ALTER TABLE public.oidb ADD note text NULL;
 
 INSERT INTO "TAP_SCHEMA"."columns" ("table_name", "column_name", "description", "unit", "ucd", "utype", "datatype", "size", "principal", "indexed", "std", "column_index") VALUES 
 ('oidb', 'dataproduct_category', 'Mainly mapped to HIERARCH DPR CATG (SCIENCE,CALIB,ACQUISITION,TECHNICAL,TEST,SIMULATION,OTHER)', '', 'meta.id', 'ObsDataset.dataProductSubtype', 'VARCHAR', '-1', '1', '0', '0', '-1');
 
 INSERT INTO "TAP_SCHEMA"."columns" ("table_name", "column_name", "description", "unit", "ucd", "utype", "datatype", "size", "principal", "indexed", "std", "column_index") VALUES 
 ('oidb', 'proposal_subid', 'Proposal sub indentifier (may reference some catalogs ids)', '', 'meta.id; obs.proposal', 'Provenance.identifier', 'VARCHAR', '-1', '1', '0', '0', '-1');
+
+INSERT INTO "TAP_SCHEMA"."columns" ("table_name", "column_name", "description", "unit", "ucd", "utype", "datatype", "size", "principal", "indexed", "std", "column_index") VALUES 
+('oidb', 'note', 'Note or remark', '', 'meta.note', 'Provenance.comment', 'VARCHAR', '-1', '1', '0', '0', '-1');
 
